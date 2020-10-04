e261b662dd9b48a943f04fec5c3e986b66136abc
WilDHuNTeR-OP <72350967+WilDHuNTeR-OP@users.noreply.github.com>
2020-10-04 23:53:36 +0530

    Add files via upload

---
+++ Hello_World/HelloWorld.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Name = "Vivek Dadhich";
+		String World = "Hello World";
+		System.out.println(Name);			
+        System.out.print(World);
+	}
+
+}
