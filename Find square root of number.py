b06071c75d533a33d221052cc904a5e0cf2f9e17
preetiadvani1 <68547797+preetiadvani1@users.noreply.github.com>
2020-10-21 22:35:36 +0530

    Create Find square root of number.py

---
+++ Find square root of number.py
@@ -0,0 +1,7 @@
+import math
+
+number = float(input(" Please Enter any numeric Value : "))
+
+squareRoot = math.sqrt(number)
+
+print("The Square Root of a Given Number {0}  = {1}".format(number, squareRoot))
