c7671026a66e8e4cdd25e67df0ee5fe713d8861d
Varun Singh <43241599+VarunSingh029@users.noreply.github.com>
2021-10-02 20:56:39 +0530

    Update kadane_algo.py
    Added maximium product subarray

--- DataStructure_and_Algorithms/Python/arrays/kadane_algo.py
+++ DataStructure_and_Algorithms/Python/arrays/kadane_algo.py
@@ -7,6 +7,8 @@ Hence, Maximum subarray sum = 7
 '''
 
 #Implementation
+
+## Maximium Subarray sum
 def maxSubArraySum(a,size): 
     max_so_far = -2**31   #initialize with minimum value
     max_ending_here = 0
@@ -28,3 +30,44 @@ print "Maximum subarray sum = ", maxSubArraySum(a,size)
 Time Complexity = O(n) as the array is traversed only once
 Space Complexity = O(1) since no extra space is used
 '''
+## Maximium product subarray
+def maxsubarrayproduct(arr):
+ 
+    n = len(arr)
+ 
+    # max positive product ending at the current position
+    max_ending_here = 1
+ 
+    # min positive product ending at the current position
+    min_ending_here = 1
+ 
+    # Initialize maximum so far
+    max_so_far = 0
+    flag = 0
+ 
+
+
+    for i in range(0, n):
+ 
+  
+        if arr[i] > 0:
+            max_ending_here = max_ending_here * arr[i]
+            min_ending_here = min (min_ending_here * arr[i], 1)
+            flag = 1
+ 
+   
+        elif arr[i] == 0:
+            max_ending_here = 1
+            min_ending_here = 1
+ 
+     
+        else:
+            temp = max_ending_here
+            max_ending_here = max (min_ending_here * arr[i], 1)
+            min_ending_here = temp * arr[i]
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+             
+    if flag == 0 and max_so_far == 0:
+        return 0
+    return max_so_far
